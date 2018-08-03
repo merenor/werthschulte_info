4ce518a899dfc601fdde0aafb96883fccb34a886
Felix Werthschulte <felix@werthschulte.info>
2018-08-03 18:13:14 +0200

    Fifth Commit

--- werthschulte_info/settings.py
+++ werthschulte_info/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
